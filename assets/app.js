0b13645b447efc9e577992891005b7d911552871
Paul Kruse <paul.kruse@posteo.de>
2024-04-14 23:42:10 +0200

    Added symfony docker

--- assets/app.js
+++ assets/app.js
@@ -9,4 +9,3 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
-import { Alert } from 'bootstrap';
